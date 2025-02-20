880316a32a0047a21e1685ba4630e0aa9d6a308a
SasmithaHiram <sasmithahiram2003@gmail.com>
2025-02-20 19:59:05 +0530

    Done Load Customer & Add Customer

--- JS/app.js
+++ JS/app.js
@@ -1,33 +1,65 @@
 function loadCustomers() {
     fetch("http://localhost:8080/customer/getAll")
-    .then(res=>res.json())
-    .then(data=> { 
-        console.log(data)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
 
-    let tableRow = `
-        <tr>
-        <th>Id</th>
-        <th>Name</th>
-        <th>Address</th>
-        <th>Salary</th>
-        </tr>`;
+            let tableRow = `<tr>
+                    <th>Name</th>
+                    <th>Address</th>
+                    <th>Salary</th>
+                    <th>Actions</th>
+                </tr>`;
 
-        let tableCustomers = document.getElementById("tblCustomers");
+            let tableCustomers = document.getElementById("tblCustomers");
 
-        data.forEach(customer => {
-        tableRow +=`
-            <tr>
-            <td>${customer.id}</td>
-            <td>${customer.name}</td>
-            <td>${customer.address}</td>
-            <td>${customer.salary}</td>
-            </tr>`
-        
-        });
-        tableCustomers.innerHTML = tableRow;
-    
-})
+            data.forEach(customer => {
+                tableRow += `<tr>
+                    <td>${customer.name}</td>
+                    <td>${customer.address}</td>
+                    <td>${customer.salary}</td>
+                    <td>
+                        <button class="btn btn-edit btn-sm"><i class="fas fa-edit"></i> Edit</button>
+                        <button class="btn btn-delete btn-sm"><i class="fas fa-trash"></i> Delete</button>
+                    </td>
+                </tr>`
+
+            });
+
+            tableCustomers.innerHTML = tableRow;
+        })
+
 }
 
 loadCustomers();
+
+function addCustomer() {
+    loadCustomers();
+    let name = document.getElementById("name").value;
+    let address = document.getElementById("address").value;
+    let salary = document.getElementById("salary").value;
+
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const raw = JSON.stringify({
+        "name": name,
+        "address": address,
+        "salary": salary
+    });
+
+    const requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow"
+    };
+
+    fetch("http://localhost:8080/customer/add", requestOptions)
+        .then((response) => response.text())
+        .then((result) => console.log(result))
+        .catch((error) => console.error(error));
+
+}
+
 
